bbf4bd0407259ae1f17be3d0934d79ab4c668bcb
SaadBinShahid <saadbinshahid.fb@gmail.com>
2014-06-24 23:29:47 +0500

    Text to Speech (Using Speech API 5)
    Simple program for Text to Speech using SAPI 5 with C#.

---
+++ myText-to-speech/Form1.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Speech;
+using System.Speech.Synthesis;
+
+namespace myText_to_speech
+{
+    public partial class Form1 : Form
+    {
+        SpeechSynthesizer myspeechSyn = new SpeechSynthesizer();
+
+        public Form1()
+        {
+            InitializeComponent();
+            label1.Text = "Developed By: Saad Bin Shahid @ Saadbinshahid.fb@gmail.com";
+        }
+
+        //exit button
+        private void button2_Click(object sender, EventArgs e)
+        {
+            myspeechSyn.Dispose();
+            Application.Exit();
+        }
+
+        //Speak myText-to-Speach
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (richTextBox1.Text != "")
+            {
+                label1.Text = "Speaking | If you cannot hear, Please check your audio settings.";
+                myspeechSyn.Volume = 100;
+                myspeechSyn.SpeakAsync(richTextBox1.Text);
+            }
+            else
+            {
+                label1.Text = "Please enter some text first !";
+            }
+        }
+
+
+    }
+}
---
+++ myText-to-speech/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("myText-to-speech")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Microsoft")]
+[assembly: AssemblyProduct("myText-to-speech")]
+[assembly: AssemblyCopyright("Copyright © Microsoft 2014")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("e985671f-385f-42de-9a62-79e7e69f8139")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
